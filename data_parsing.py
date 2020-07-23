b9b4d61249edde4d021e5ad3782ec0340cb76835
rebecca-my <rebecca.sc.myers@gmail.com>
2020-07-22 22:54:23 -0400

    lstm and cluster updates

--- data_parsing.py
+++ data_parsing.py
@@ -12,7 +12,7 @@ def get_trajectory(df):
     order.append(0)
     return order
 
-def load_trajectories(folder_path):
+def load_trajectories(folder_path, status_file):
 	trajectories = []
 
 	for fname in glob(folder_path + '/*.csv'):
@@ -20,4 +20,15 @@ def load_trajectories(folder_path):
 	    trajectories.append(df)
 	    
 	trajectories = pd.concat(trajectories)
-	return trajectories.groupby('user_id').apply(get_trajectory)
+	trajectories = pd.DataFrame(trajectories.groupby('user_id').apply(get_trajectory)).reset_index()
+
+	id_and_performance = pd.read_csv(status_file)
+	dummy = pd.merge(trajectories, id_and_performance, left_on='user_id', right_on='id', how='inner')
+
+	#add data for pass/fail to status
+	status = np.where(dummy['certGrp'].str.contains('Certified'), 1, 0)
+	trajectories = trajectories.set_index('user_id')
+	trajectories.columns = ['some string']
+	return trajectories['some string'], status
+
+#def load_status(folder_path):
